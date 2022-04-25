1fb15ab9210ce5e846491cca0439c989a5dcc6d0
simonhangarita <101430109+simonhangarita@users.noreply.github.com>
2022-04-25 15:52:27 -0500

    Create zona.py

---
+++ gestion/zoologico/zona.py
@@ -0,0 +1,25 @@
+class Zona:
+  def __init__(self,nombre,zoo):
+    self._nombre=nombre
+    self._zoo=zoo
+    self._animales=[]
+  def getNombre(self):
+    return self._nombre
+  def getZoo(self):
+    return self._zoo
+  def getAnimales(self):
+    return self._animales
+  def setNombre(self,nombre):
+    self._nombre=nombre
+  def setZoo(self,nombre):
+    self._zoo=nombre
+  def setAnimales(self,nombre):
+    self._animales=nombre
+  def agregarAnimales(self,animal):
+    self._animales.append(animal)
+  def cantidadAnimales(self):
+    x=0
+    for i in self._animales:
+      if (str(type(i).__name__)=="Animal"):
+        x+=1
+    return x 
