92fb60cea911157afbae4d8526e9d28a4c442465
simonhangarita <101430109+simonhangarita@users.noreply.github.com>
2022-04-25 17:06:30 -0500

    Update zoologico.py

--- gestion/zoologico.py
+++ gestion/zoologico.py
@@ -1,6 +1,6 @@
 from gestion.zona import Zona
 class Zoologico:
-  def __init__(self,nombre,ubicacion):
+  def __init__(self,nombre=None,ubicacion=None):
     self._nombre=nombre
     self._ubicacion=ubicacion
     self._zonas=[]
